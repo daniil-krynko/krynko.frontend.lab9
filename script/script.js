650696544d243b6b26ff0b189c9a56ce2f5b27b4
Daniil Krynko <daniil.krynko@gmail.com>
2022-10-30 18:37:47 +0200

    All done

--- script/script.js
+++ script/script.js
@@ -1,17 +1,21 @@
-let K = prompt("Enter K");
-let size = 30;
-const arr = [];
-for (let i = 0; i < size; i++) {
-    arr.push(Math.floor(Math.random() * 101 - 50));
+function search() {
+    let K = prompt("Enter K");
+    let size = 20;
+    const arr = [];
+    for (let i = 0; i < size; i++) {
+        arr.push(Math.floor(Math.random() * 10));
+    }
+    console.log(arr);
+    let index = 0;
+    while (index < size) {
+        if (K == arr[index]) {
+            document.getElementById("Number").innerText = `${index}`;
+            document.getElementById("Data").innerText = `${arr[index]}`;
+            return;
+        }
+        index++;
+    }
+    alert("K not found");
 }
 
-let index = 0;
-while (index < size) {
-    if (K === arr[index]) {
-        document.getElementById("Number").innerText = `${index}:`;
-        document.getElementById("Data").innerText = `${arr[index]}`;
-        exit();
-    }
-    index++;
-}
-alert("K not found");
+search();
