3d1865058c3950bb42ed0cfe654b39aaa41bce11
Daniil Krynko <daniil.krynko@gmail.com>
2022-10-30 19:11:25 +0200

    All done

--- script/script.js
+++ script/script.js
@@ -1,21 +1,6 @@
-function search() {
-    let K = prompt("Enter K");
-    let size = 20;
-    const arr = [];
-    for (let i = 0; i < size; i++) {
-        arr.push(Math.floor(Math.random() * 10));
-    }
-    console.log(arr);
-    let index = 0;
-    while (index < size) {
-        if (K == arr[index]) {
-            document.getElementById("Number").innerText = `${index}`;
-            document.getElementById("Data").innerText = `${arr[index]}`;
-            return;
-        }
-        index++;
-    }
-    alert("K not found");
+function splitEmail() {
+    let email = document.getElementById("UserEmail").value;
+    document.getElementById("Before").innerText = email.split("@")[0];
+    document.getElementById("Before").style.color = "red";
+    document.getElementById("After").innerText = '@' + email.split("@")[1];
 }
-
-search();
