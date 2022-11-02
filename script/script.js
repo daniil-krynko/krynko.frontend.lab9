2d3cacefd35ae4b22d4ea69a9f97e0b8b6b59a30
Daniil Krynko <daniil.krynko@gmail.com>
2022-11-02 21:00:56 +0200

    Responsive flex is ready. Circles are not ideal. No script

--- script/script.js
+++
@@ -1,6 +0,0 @@
-function splitEmail() {
-    let email = document.getElementById("UserEmail").value;
-    document.getElementById("Before").innerText = email.split("@")[0];
-    document.getElementById("Before").style.color = "red";
-    document.getElementById("After").innerText = '@' + email.split("@")[1];
-}
